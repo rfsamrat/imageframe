c8f981d1064f9aebc4c9048400ae23855e98950a
rfsamrat <rfsamrat@gmail.com>
2022-09-04 09:58:04 -0400

    wwithout responsive

---
+++ node.js
@@ -0,0 +1,492 @@
+// function extraBigDevice(){
+//     var table = document.getElementById("myTable");
+    
+//     var row = table.insertRow(0);
+  
+//                 var cell0 = row.insertCell(0);
+//                 var timage= document.createElement('img');
+//                 cell0.appendChild(timage);
+//                 timage.src="1.jpg";
+//                 // timage.height="100%";
+//                 //timage.width="100%"
+      
+      
+//               var cell1 = row.insertCell(1);
+//               var timage= document.createElement('img');
+//               cell1.appendChild(timage);
+//               timage.src="2.jpg";
+  
+     
+//               var cell2 = row.insertCell(2);
+//               var timage= document.createElement('img');
+//               cell2.appendChild(timage);
+//               timage.src="3.jpg";
+              
+//               var cell3 = row.insertCell(3);
+//               var timage= document.createElement('img');
+//               cell3.appendChild(timage);
+//               timage.src="4.jpg";
+             
+  
+  
+//               var cell4 = row.insertCell(4);
+//               var timage= document.createElement('img');
+//               cell4.appendChild(timage);
+//               timage.src="5.jpg";
+            
+    
+    
+    
+//               var cell5 = row.insertCell(5);
+//               var timage= document.createElement('img');
+//               cell5.appendChild(timage);
+//               timage.src="6.jpg";
+     
+    
+      
+      
+//               var cell6 = row.insertCell(6);
+//               var timage= document.createElement('img');
+//               cell6.appendChild(timage);
+//               timage.src="7.jpg";
+      
+//               var cell7 = row.insertCell(7);
+//               var timage= document.createElement('img');
+//               cell7.appendChild(timage);
+//               timage.src="7.jpg";
+             
+  
+//               var cell8 = row.insertCell(8);
+//               var timage= document.createElement('img');
+//               cell8.appendChild(timage);
+//               timage.src="8.jpg";
+              
+      
+//               var cell9 = row.insertCell(9);
+//               var timage= document.createElement('img');
+//                cell9.appendChild(timage);
+//               timage.src="9.jpg";
+
+
+              
+//  //second row started 
+//        var secondRow = table.insertRow(1);  
+//               var cell10 = secondRow.insertCell(0);
+//               var timage= document.createElement('img');
+//               cell10.appendChild(timage);
+//               timage.src="10.jpg";  
+              
+//               var cell11 = secondRow.insertCell(1);
+//              cell11.setAttribute("id", "special-td");
+//             // document.getElementById("special-td").rowSpan = "1";
+//              document.getElementById("special-td").colSpan = "8";
+  
+//              cell11.innerHTML="<p>Lorem, ipsum dolor sit amet consectetur adipisicing elit. Magni nobis commodi vero asperiores reprehenderit? Aperiam excepturi repellat tempora deleniti, mollitia illo, distinctio fugit officia architecto dolor repudiandae eos aliquam quod!</p>";
+        
+             
+//              var cell12 = secondRow.insertCell(2);
+//               var timage= document.createElement('img');
+//               cell12.appendChild(timage);
+//               timage.src="11.jpg";  
+  
+  
+
+//       //third row
+//       var row = table.insertRow(2);
+  
+//   var cell0 = row.insertCell(0);
+//   var timage= document.createElement('img');
+//   cell0.appendChild(timage);
+//   timage.src="1.jpg";
+//   // timage.height="100%";
+//   //timage.width="100%"
+  
+  
+//           var cell1 = row.insertCell(1);
+//           var timage= document.createElement('img');
+//           cell1.appendChild(timage);
+//           timage.src="2.jpg";
+  
+  
+//           var cell2 = row.insertCell(2);
+//           var timage= document.createElement('img');
+//           cell2.appendChild(timage);
+//           timage.src="3.jpg";
+          
+//           var cell3 = row.insertCell(3);
+//           var timage= document.createElement('img');
+//           cell3.appendChild(timage);
+//           timage.src="4.jpg";
+         
+  
+  
+//           var cell4 = row.insertCell(4);
+//           var timage= document.createElement('img');
+//           cell4.appendChild(timage);
+//           timage.src="5.jpg";
+        
+  
+  
+  
+//           var cell5 = row.insertCell(5);
+//           var timage= document.createElement('img');
+//           cell5.appendChild(timage);
+//           timage.src="6.jpg";
+  
+  
+  
+  
+//           var cell6 = row.insertCell(6);
+//           var timage= document.createElement('img');
+//           cell6.appendChild(timage);
+//           timage.src="7.jpg";
+  
+//           var cell7 = row.insertCell(7);
+//           var timage= document.createElement('img');
+//           cell7.appendChild(timage);
+//           timage.src="7.jpg";
+         
+  
+//           var cell8 = row.insertCell(8);
+//           var timage= document.createElement('img');
+//           cell8.appendChild(timage);
+//           timage.src="8.jpg";
+
+//           var cell9 = row.insertCell(9);
+//           var timage= document.createElement('img');
+//            cell9.appendChild(timage);
+//           timage.src="9.jpg";
+
+
+// }
+
+function bigDevice(){
+        
+
+
+    var table = document.getElementById("myTable");
+    var row = table.insertRow(0);
+  
+      var cell1 = row.insertCell(0);
+      var timage= document.createElement('img');
+      cell1.appendChild(timage);
+      timage.src="1.jpg";
+     // timage.height="100%";
+      //timage.width="100%"
+      
+      
+              var cell1 = row.insertCell(1);
+              var timage= document.createElement('img');
+              cell1.appendChild(timage);
+              timage.src="";
+  
+     
+              var cell2 = row.insertCell(2);
+              var timage= document.createElement('img');
+              cell2.appendChild(timage);
+              timage.src="3.jpg";
+              
+              var cell3 = row.insertCell(3);
+              var timage= document.createElement('img');
+              cell3.appendChild(timage);
+              timage.src="4.jpg";
+             
+  
+  
+              var cell4 = row.insertCell(4);
+              var timage= document.createElement('img');
+              cell4.appendChild(timage);
+              timage.src="5.jpg";
+            
+    
+    
+    
+              var cell5 = row.insertCell(5);
+              var timage= document.createElement('img');
+              cell5.appendChild(timage);
+              timage.src="6.jpg";
+     
+    
+      
+      
+              var cell6 = row.insertCell(6);
+              var timage= document.createElement('img');
+              cell6.appendChild(timage);
+              timage.src="7.jpg";
+      
+              var cell7 = row.insertCell(7);
+              var timage= document.createElement('img');
+              cell7.appendChild(timage);
+              timage.src="7.jpg";
+             
+  
+              var cell8 = row.insertCell(8);
+              var timage= document.createElement('img');
+              cell8.appendChild(timage);
+              timage.src="8.jpg";
+              
+      
+              // var cell9 = row.insertCell(9);
+              // var timage= document.createElement('img');
+              // cell9.appendChild(timage);
+              // timage.src="9.jpg";
+              
+              //new row started 
+       var secondRow = table.insertRow(1);  
+              var cell10 = secondRow.insertCell(0);
+              var timage= document.createElement('img');
+              cell10.appendChild(timage);
+              timage.src="10.jpg";  
+              
+              var cell11 = secondRow.insertCell(1);
+             cell11.setAttribute("id", "special-td");
+             document.getElementById("special-td").rowSpan = "2";
+             document.getElementById("special-td").colSpan = "7";
+  
+             cell11.innerHTML="<p>Lorem, ipsum dolor sit amet consectetur adipisicing elit. Magni nobis commodi vero asperiores reprehenderit? Aperiam excepturi repellat tempora deleniti, mollitia illo, distinctio fugit officia architecto dolor repudiandae eos aliquam quod!</p>";
+        
+             
+             var cell12 = secondRow.insertCell(2);
+              var timage= document.createElement('img');
+              cell12.appendChild(timage);
+              timage.src="11.jpg";  
+  
+  
+  
+      //third row
+  
+      var thirdRow = table.insertRow(2);  
+              var cell13 = thirdRow.insertCell(0);
+              var timage= document.createElement('img');
+              cell13.appendChild(timage);
+              timage.src="12.jpg";  
+              
+              
+        
+             
+             var cell14 = thirdRow.insertCell(1);
+              var timage= document.createElement('img');
+              cell14.appendChild(timage);
+              timage.src="16.jpg";  
+      //fourth row
+      var row = table.insertRow(3);
+  
+  var cell1 = row.insertCell(0);
+  var timage= document.createElement('img');
+  cell1.appendChild(timage);
+  timage.src="1.jpg";
+  // timage.height="100%";
+  //timage.width="100%"
+  
+  
+          var cell1 = row.insertCell(1);
+          var timage= document.createElement('img');
+          cell1.appendChild(timage);
+          timage.src="2.jpg";
+  
+  
+          var cell2 = row.insertCell(2);
+          var timage= document.createElement('img');
+          cell2.appendChild(timage);
+          timage.src="3.jpg";
+          
+          var cell3 = row.insertCell(3);
+          var timage= document.createElement('img');
+          cell3.appendChild(timage);
+          timage.src="4.jpg";
+         
+  
+  
+          var cell4 = row.insertCell(4);
+          var timage= document.createElement('img');
+          cell4.appendChild(timage);
+          timage.src="5.jpg";
+        
+  
+  
+  
+          var cell5 = row.insertCell(5);
+          var timage= document.createElement('img');
+          cell5.appendChild(timage);
+          timage.src="6.jpg";
+  
+  
+  
+  
+          var cell6 = row.insertCell(6);
+          var timage= document.createElement('img');
+          cell6.appendChild(timage);
+          timage.src="7.jpg";
+  
+          var cell7 = row.insertCell(7);
+          var timage= document.createElement('img');
+          cell7.appendChild(timage);
+          timage.src="7.jpg";
+         
+  
+          var cell8 = row.insertCell(8);
+          var timage= document.createElement('img');
+          cell8.appendChild(timage);
+          timage.src="8.jpg";
+      }
+  
+
+
+
+
+function mediumDevice(){
+
+
+    var table = document.getElementById("myTable");
+    var row = table.insertRow(0);
+  
+      var cell1 = row.insertCell(0);
+      var timage= document.createElement('img');
+      cell1.appendChild(timage);
+      timage.src="1.jpg";
+ 
+      
+      
+              var cell1 = row.insertCell(1);
+              var timage= document.createElement('img');
+              cell1.appendChild(timage);
+              timage.src="";
+  
+     
+              var cell2 = row.insertCell(2);
+              var timage= document.createElement('img');
+              cell2.appendChild(timage);
+              timage.src="3.jpg";
+              
+              var cell3 = row.insertCell(3);
+              var timage= document.createElement('img');
+              cell3.appendChild(timage);
+              timage.src="4.jpg";
+             
+  
+  
+              var cell4 = row.insertCell(4);
+              var timage= document.createElement('img');
+              cell4.appendChild(timage);
+              timage.src="5.jpg";
+            
+    
+    
+    
+              var cell5 = row.insertCell(5);
+              var timage= document.createElement('img');
+              cell5.appendChild(timage);
+              timage.src="6.jpg";
+     
+    
+      
+      
+              var cell6 = row.insertCell(6);
+              var timage= document.createElement('img');
+              cell6.appendChild(timage);
+              timage.src="7.jpg";
+      
+              var cell7 = row.insertCell(7);
+              var timage= document.createElement('img');
+              cell7.appendChild(timage);
+              timage.src="7.jpg";
+             
+  
+
+              
+     //new row started 
+       var secondRow = table.insertRow(1);  
+              var cell10 = secondRow.insertCell(0);
+              var timage= document.createElement('img');
+              cell10.appendChild(timage);
+              timage.src="10.jpg";  
+              
+              var cell11 = secondRow.insertCell(1);
+             cell11.setAttribute("id", "special-td");
+             document.getElementById("special-td").rowSpan = "2";
+             document.getElementById("special-td").colSpan = "6";
+  
+             cell11.innerHTML="<p>Lorem, ipsum dolor sit amet consectetur adipisicing elit. Magni nobis commodi vero asperiores reprehenderit? Aperiam excepturi repellat tempora deleniti, mollitia illo, distinctio fugit officia architecto dolor repudiandae eos aliquam quod!</p>";
+        
+             
+             var cell12 = secondRow.insertCell(2);
+              var timage= document.createElement('img');
+              cell12.appendChild(timage);
+              timage.src="11.jpg";  
+  
+  
+  
+      //third row
+  
+      var thirdRow = table.insertRow(2);  
+              var cell13 = thirdRow.insertCell(0);
+              var timage= document.createElement('img');
+              cell13.appendChild(timage);
+              timage.src="12.jpg";  
+              
+              
+        
+             
+             var cell14 = thirdRow.insertCell(1);
+              var timage= document.createElement('img');
+              cell14.appendChild(timage);
+              timage.src="16.jpg";  
+      //fourth row
+      var row = table.insertRow(3);
+  
+  var cell1 = row.insertCell(0);
+  var timage= document.createElement('img');
+  cell1.appendChild(timage);
+  timage.src="1.jpg";
+  // timage.height="100%";
+  //timage.width="100%"
+  
+  
+          var cell1 = row.insertCell(1);
+          var timage= document.createElement('img');
+          cell1.appendChild(timage);
+          timage.src="2.jpg";
+  
+  
+          var cell2 = row.insertCell(2);
+          var timage= document.createElement('img');
+          cell2.appendChild(timage);
+          timage.src="3.jpg";
+          
+          var cell3 = row.insertCell(3);
+          var timage= document.createElement('img');
+          cell3.appendChild(timage);
+          timage.src="4.jpg";
+         
+  
+  
+          var cell4 = row.insertCell(4);
+          var timage= document.createElement('img');
+          cell4.appendChild(timage);
+          timage.src="5.jpg";
+        
+  
+  
+  
+          var cell5 = row.insertCell(5);
+          var timage= document.createElement('img');
+          cell5.appendChild(timage);
+          timage.src="6.jpg";
+  
+  
+  
+  
+          var cell6 = row.insertCell(6);
+          var timage= document.createElement('img');
+          cell6.appendChild(timage);
+          timage.src="7.jpg";
+  
+          var cell7 = row.insertCell(7);
+          var timage= document.createElement('img');
+          cell7.appendChild(timage);
+          timage.src="7.jpg";
+         
+  
+
+
+}
